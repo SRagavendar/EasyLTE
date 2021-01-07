628ff65e1ee865b3eb278afc359c11d4e8ebcbbc
SRagavendar <ragajks@gmail.com>
2021-01-07 12:50:43 +0530

    Modified: script for eNodeB start

--- srsLTE/start-enb.sh
+++ srsLTE/start-enb.sh
@@ -3,6 +3,7 @@
 set -ux
 tmux new-session -d -s enb
 tmux send-keys 'sudo srsenb /home/ses002/EasyLTE/config/srsenb/enb.conf' C-m
+sleep 1.0
 tmux split-window -v
 tmux select-layout even-vertical
 tmux attach-session -d -t enb
